0a25a66a8cdd5ad42ccb6a8b1fe3a6d11a34c1bf
Johannes Skjærbæk <john@particledesigns.dk>
2017-03-27 15:26:09 +0200

    payment js/css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 /* Created by Johannes & Ralf. */
+
+console.log("Stuff.");
+
+jQuery("document").ready(function(){
+    var $ = jQuery.noConflict();
+
+    $(".payment-button button").click(function(){
+        console.log("wot");
+        $("#payment").css("display", "flex");
+    });
+});
